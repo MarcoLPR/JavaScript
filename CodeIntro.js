ca92b35b357ed18258c7170274c0a8be72705d7c
Leonardi Pérez <marcolpr98@gmail.com>
2017-02-21 16:20:51 -0600

    Update CodeIntro.js

--- CodeIntro.js
+++ CodeIntro.js
@@ -28,11 +28,15 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-//Aqui como podria generar un cuestionario? intente meter If (answer) pero no :P
 rl.question('What is your name? ', (answer) => {
   console.log(`Welcome back: ${answer}`);
-
+  function Reversal(meow) {
+    var backward = "";
+    for (var i = meow.length-1; i > -1; i--) {
+      backward += (meow[i]);
+    }
+      return backward;
+  }
+  console.log(Reversal(answer));
   rl.close();
-
 });
-
