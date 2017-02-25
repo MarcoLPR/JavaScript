5d35891e8748e68f24735bc14f2e554ff706a6dc
Leonardi Pérez <marcolpr98@gmail.com>
2017-02-25 17:29:42 -0600

    Update CodeIntro.js

--- CodeIntro.js
+++ CodeIntro.js
@@ -1,28 +1,8 @@
-function DarValor(letra) {
-if (letra == "E" || letra == "e"){
-return "3"
-}
-else if (letra == "O" || letra == "o"){
-return "0"
-}
-else if (isNaN(letra) == false) {
-return ++letra;
-}
-else {
-return letra
-}
-}
-function Invertir(yeyo) {
-  var invertidoooooooooo = "";
-  for (var i = yeyo.length-1; i > -1; i--) {
-    invertidoooooooooo += DarValor(yeyo[i]);
-  }
-    return invertidoooooooooo;
-}
-
-console.log(Invertir("YEYO555"));
-console.log(Invertir("Yeyo123"));
-//Input
+function Reversal(meow) {
+  var backward = "";
+  for (var i = meow.length-1; i > -1; i--) {
+    backward += (meow[i]);
+}  return backward; }
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -30,13 +10,6 @@ const rl = readline.createInterface({
 });
 rl.question('What is your name? ', (answer) => {
   console.log(`Welcome back: ${answer}`);
-  function Reversal(meow) {
-    var backward = "";
-    for (var i = meow.length-1; i > -1; i--) {
-      backward += (meow[i]);
-    }
-      return backward;
-  }
   console.log(Reversal(answer));
   rl.close();
 });
