b876cfcfa275dd93ee4291e49ce81f16f47c9da2
Leonardi Pérez <marcolpr98@gmail.com>
2017-02-25 19:05:26 -0600

    Update CodeIntro.js

--- CodeIntro.js
+++ CodeIntro.js
@@ -1,3 +1,6 @@
+var hello = "Welcome";
+var chalk = require('chalk');
+console.log(chalk.green(hello));
 function Reversal(meow) {
   var backward = "";
   for (var i = meow.length-1; i > -1; i--) {
@@ -8,8 +11,12 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-rl.question('What is your name? ', (answer) => {
-  console.log(`Welcome back: ${answer}`);
-  console.log(Reversal(answer));
+rl.question(chalk.blue('What is your name? '), (answer) => {
+  console.log(chalk.red(`Welcome back: ${answer}`));
+  console.log(chalk.green(Reversal(answer)));
   rl.close();
+  var morning = "Have an awesome day!";
+  var bye = "See you later " + (answer);
+  console.log(chalk.red(morning));
+  console.log(chalk.blue(bye));
 });
