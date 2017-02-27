f7744c33b8c4fd2232da343c7c398b479ddf92fa
Leonardi Pérez <marcolpr98@gmail.com>
2017-02-26 18:38:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var hello = "Welcome";
 var chalk = require('chalk');
 var reversal = require("./reversal");
+var fs = require('fs');
 console.log(chalk.green(hello));
 const readline = require('readline');
 
@@ -11,12 +12,22 @@ const rl = readline.createInterface({
 
 rl.question(chalk.blue('What is your name? '), (answer1) => {
     rl.question(chalk.red('What is your lastname?  '), (answer2) => {
-        var result = (answer1) + " " + (answer2);
+        var firstName = answer1;
+        var lastName = answer2;
+        var result = (firstName) + " " + (lastName);
         rl.close();
         console.log(reversal(`${result}`));
         var morning = "Have an awesome day!";
         var bye = `See you later ${result}`;
         console.log(chalk.green(morning));
         console.log(chalk.blue(bye));
+          var user = new Object();
+          user.firstName = firstName;
+          user.lastName = lastName;
+          var myString = JSON.stringify(user);
+          fs.writeFile('./user.json', myString, function(err){
+  if (err) return console.error(err);
+    console.log ('done')
+});
     });
 });
