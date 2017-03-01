2c3fddb253a4ba2783c4343df737b22e07568682
Leonardi Pérez <marcolpr98@gmail.com>
2017-03-01 17:06:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,33 @@
-var hello = "Welcome";
 var chalk = require('chalk');
 var reversal = require("./reversal");
 var fs = require('fs');
 var menu = require('node-menu');
-console.log(chalk.green(hello));  
-menu.addDelimiter('-', 40, 'Main Menu')
-  .addItem(
-        'Write userfile',
-        function() {
-          const readline = require('readline');
-          const rl = readline.createInterface({
-              input: process.stdin,
-              output: process.stdout
-              });
-        rl.question(chalk.blue('What is your name? '), (answer1) => {
-            rl.question(chalk.red('What is your lastname?  '), (answer2) => {
-                var firstName = answer1;
-                var lastName = answer2;
-                var result = (firstName) + " " + (lastName);
-                rl.close();
-                console.log(reversal(`${result}`));
-                var morning = "Have an awesome day!";
-                var bye = `See you later ${result}`;
-                console.log(chalk.green(morning));
-                console.log(chalk.blue(bye));
-                  var user = new Object();
-                  user.firstName = firstName;
-                  user.lastName = lastName;
+menu.disableDefaultHeader();
+menu.addDelimiter('-', 40, 'Main Menu'),
+menu.addItem(
+       'Write userfile',
+       function(firstName, lastName) {
+        console.log('What is your name? '), firstName => {
+        console.log('What is your lastname?  '), lastName => {
+            var firstName = firstName;
+            var lastName = lastName;
+              var user = new Object();
+                 user.firstName = firstName;
+                 user.lastName = lastName;
                   var myString = JSON.stringify(user);
                   fs.writeFile('./user.json', myString, function(err){
           if (err) return console.error(err);
             console.log ('done')
-        });
-            });
-        })
-  })
-  .addItem(
+          },
+null,
+[
+  {'name': 'firstName', 'type': 'string'},
+  {'name': 'lastName', 'type': 'string'}
+]);
+}}})
+menu.addItem(
         "Read userfile",
-        function() {
+        function(read) {
         fs.readFile('./user.json', 'utf8', function (err,data) {
           if (err) {
             return console.log(err);
@@ -45,5 +35,5 @@ menu.addDelimiter('-', 40, 'Main Menu')
             console.log(data);
 })
 })
-.addDelimiter('-', 40)
-.start();
+menu.addDelimiter('*', 40),
+menu.start();
