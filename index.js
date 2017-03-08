af7b75b8c90ed37b2c8b96fe1489370966c6f0b7
Leonardi Pérez <marcolpr98@gmail.com>
2017-03-07 20:41:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,37 @@ var chalk = require('chalk');
 var reversal = require("./reversal");
 var fs = require('fs');
 var menu = require('node-menu');
+var askLast = 'What is your lastname?';
 menu.disableDefaultHeader();
 menu.addDelimiter('-', 40, 'Main Menu'),
 menu.addItem(
-       'Write userfile',
-       function(firstName, lastName) {
-        console.log('What is your name? '), firstName => {
-        console.log('What is your lastname?  '), lastName => {
-            var firstName = firstName;
+      'Write userfile',
+       function question1(askName, firstName) {
+        var askName = 'What is your name?';
+        console.log(askName), firstName;
+         var firstName = firstName;
+          function question2(askLast, lastName) {
+            var askLast = 'What is your lastname?';
+            console.log(askLast), lastName;
             var lastName = lastName;
               var user = new Object();
-                 user.firstName = firstName;
-                 user.lastName = lastName;
-                  var myString = JSON.stringify(user);
-                  fs.writeFile('./user.json', myString, function(err){
-          if (err) return console.error(err);
-            console.log ('done')
-          },
+               user.firstName = firstName;
+               user.lastName = lastName;
+                   var myString = JSON.stringify(user);
+                   fs.writeFile('./user.json', myString, function(err){
+           if (err) return console.error(err);
+             console.log ('done')
+           },
 null,
 [
-  {'name': 'firstName', 'type': 'string'},
-  {'name': 'lastName', 'type': 'string'}
+  {'name': 'firstName', 'type': "string"},
+  {'name': 'lastName', 'type': "string"}
 ]);
-}}})
+}})
 menu.addItem(
-        "Read userfile",
-        function(read) {
-        fs.readFile('./user.json', 'utf8', function (err,data) {
+  "Read userfile",
+   function(read) {
+     fs.readFile('./user.json', 'utf8', function (err,data) {
           if (err) {
             return console.log(err);
             }
