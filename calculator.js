4ac3cef90dd5f461be646a90b432e7fdb61dd60a
Leonardi Pérez <marcolpr98@gmail.com>
2017-03-22 18:57:04 -0600

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,18 @@
+module.export = {
+  sumNumbers: function(x, y) {
+  var result = (x + y);
+  console.log( chalk.red(result));
+  },
+  minusNumbers: function(x, y) {
+    var result = (x - y);
+    console.log( chalk.blue(result));
+  },
+  multiplicateNumbers: function(x, y) {
+    var result = (x * y);
+    console.log( chalk.green(result));
+  },
+  divideNumbers: function(x, y) {
+    var result = (x / y);
+    console.log( chalk.yellow (result));
+  }
+};
