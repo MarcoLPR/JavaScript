52f31ec0a4205eadb351748eca49ed04bdedda49
Leonardi Pérez <marcolpr98@gmail.com>
2017-03-26 17:12:31 -0600

    Update geolocation.js

--- geolocation.js
+++ geolocation.js
@@ -1,17 +1,16 @@
 var http = require('http');
 var options = {
 host: 'ip-api.com',
-path: '/json/?fields=country,countryCode,region,regionName,city,zip,lat,lon,timezone'
+path: '/json/'
 };
 module.exports = {
- locate: function() {
+ locate: function(ip) {
+     options.path = options.path + ip;
    callback = function(response) {
     var str = '';
-
     response.on('data', function (chunk) {
       str += chunk;
     });
-
     response.on('end', function () {
       console.log(str);
     });
