43adff34a3d76788dffd9a419eaca9d1384eb6c4
Leonardi Pérez <marcolpr98@gmail.com>
2017-04-14 15:05:17 -0500

    Finally works

--- webapp.js
+++ webapp.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
-  $('.input1').keypress(function (e) {
-  if (e.which == 13) {
-  $('.input1').val();
-  var $ip = $('.input1');
+  $('.button').click(function () {
+  var $ip = $('.input1').val();
   var ipAPI = "/location/";
   var $consulted = $('#consultedIP');
   $.getJSON( ipAPI + $ip, function(data) {
-    $consulted.val(data);
-    return false;
+    var myString = JSON.stringify(data);
+    $consulted.text(myString);
   })
-}}
-)}
-);
+  })
+});
