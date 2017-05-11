fd8d3de96d26b82d33c790aa7698c3ca543fe171
Leonardi Pérez <marcolpr98@gmail.com>
2017-05-10 22:31:33 -0500

    POST request using Jquery, first try to PUT and DELETE ajax

--- wwwroot/webdb.js
+++ wwwroot/webdb.js
@@ -1,5 +1,6 @@
+
 $(document).ready(function() {
-  $('.button').click(function () {
+  $('.button1').click(function () {
   var $slaveID = $('.input1').val();
   var slave = "/slave/";
   $.getJSON( slave + $slaveID, function(data) {
@@ -11,5 +12,53 @@ $(document).ready(function() {
     $('.city').val(data[0].city);
     $('.email').val(data[0].email);
   }
-})})
+})});
+ $('.button2').click(function () {
+   var name = $('.name').val();
+   var number = $('.number').val();
+   var city = $('.city').val();
+   var email = $('.email').val();
+   var entry = {
+     "name": name,
+     "number": number,
+     "city": city,
+     "email": email
+   }
+   var entry = JSON.stringify(entry);
+   $.post({
+  url: "/slave",
+  data: entry,
+  contentType: 'application/json',
+  success: function(response,status) {
+    alert("Received")
+}});
 });
+$('.button3').click(function () {
+  var row = $('.input1').val();
+  var name = $('.name').val();
+  var number = $('.number').val();
+  var city = $('.city').val();
+  var email = $('.email').val();
+  var change = {
+    "name": name,
+     "number": number,
+     "city": city,
+     "email": email
+   }
+  $.ajax({
+  url: "/slave" + row,
+  type: 'put',
+  data: change,
+  contentType: 'application/json',
+  success: function(response,status) {
+    alert("Received")
+}})})
+});
+$('.button4').click(function () {
+  var row = $('.input1').val();
+  $.ajax({
+  url: "/slave" + row,
+  type: 'DELETE',
+  success: function(response,status) {
+  alert("Received")
+  }})});
