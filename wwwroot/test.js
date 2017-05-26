a76b63e68368382df4d4866893b3f47dfc113d8c
Leonardi Pérez <marcolpr98@gmail.com>
2017-05-25 19:31:23 -0500

    Creation of test.js and first attempt to work with Karma

---
+++ wwwroot/test.js
@@ -0,0 +1,10 @@
+describe("Should return 2", function(){
+    var a = 1;
+    var b = 1;
+
+    it("Sum numbers", function() {
+        var result = a + b;
+
+    expect(result).toBe(2);
+    });
+});
